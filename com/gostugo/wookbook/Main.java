76c85dfbbd2794bf3d7133de42d2a35a7c736ab1
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2022-03-24 21:59:11 -0500

    [sm] relearning imports and doing a littler exercise in var names

--- com/gostugo/wookbook/Main.java
+++ com/gostugo/wookbook/Main.java
@@ -1,6 +1,26 @@
 package com.gostugo.wookbook;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Main {
+
+    // BAD:
+    int d; // elapsed time in days
+
+    // GOOD:
+    int elapsedTimeInDays;
+    int daysSinceCreation;
+    int daysSinceModification;
+    int fileAgeInDays;
+
+    public List<int[]> getFlaggedCells() {
+        List<int[]> flaggedCells = new ArrayList<int[]>();
+        for (int[] cell : gameBoard)
+            if (cell[STATUS_VALUE] == FLAGGED)
+            flaggedCells.add(cell);
+        return flaggedCells;
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
