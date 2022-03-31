ef065c59b4af5d067f75fcbf2e64b37b3121d3c0
Stuart Marsh <stujmar@gmail.com>
2022-03-30 20:04:27 -0500

    [sm] added cleaned up version of this HTMLUtil function from the clean code book

--- com/gostugo/wookbook/HtmlUtil.java
+++ com/gostugo/wookbook/HtmlUtil.java
@@ -1,3 +1,5 @@
+// Before
+
 public static String testableHtml(
 PageData pageData,
 boolean includeSuiteSetup
@@ -42,4 +44,19 @@ buffer.append("\n")
 .append("!include -teardown .")
 .append(tearDownPathName)
 .append("\n");
+}
+
+// After
+
+public static String renderPageWithSetupsAndTeardowns( PageData pageData, boolean isSuite) throws Exception {
+  boolean isTestPage = pageData.hasAttribute("Test");
+  if (isTestPage) {
+    WikiPage testPage = pageData.getWikiPage();
+    StringBuffer newPageContent = new StringBuffer();
+    includeSetupPages(testPage, newPageContent, isSuite);
+    newPageContent.append(pageData.getContent());
+    includeTeardownPages(testPage, newPageContent, isSuite);
+    pageData.setContent(newPageContent.toString());
+  }
+  return pageData.getHtml();
 }
