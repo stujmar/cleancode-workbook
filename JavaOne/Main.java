d82a3cf95b27cda81099cf18404c567496256507
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2022-04-11 22:32:28 -0500

    [sm] added a scanner to deal with user input a little

--- JavaOne/Main.java
+++ JavaOne/Main.java
@@ -1,8 +1,35 @@
 package JavaOne;
 
+import java.util.Scanner;
+
 public class Main {
   
   public static void main(String[] args) {
+    int studentAge = 15;
+    double studentGPA = 3.45;
+    boolean isStudentGraduated = true;
+    String studentName = "John Smith";
+    String firstName = "John";
+    String lastName = "Smith";
+    char firstInitial = firstName.charAt(0);
+    char lastInitial = lastName.charAt(0);
+    String initials = firstInitial + ". " + lastInitial + ". " + "has GPA of " + studentGPA;
+
     System.out.println("Hello World! I like tacos.");
+    System.out.println(studentAge);
+    System.out.println(studentGPA);
+    System.out.println(isStudentGraduated);
+    System.out.println(studentName);
+    System.out.println(firstInitial);
+    System.out.println(lastInitial);
+    System.out.println(initials);
+
+    System.out.println("Enter your GPA: ");
+    Scanner input = new Scanner(System.in);
+    studentGPA = input.nextDouble();
+    System.out.println("Your GPA is " + studentGPA);
+    input.close();
+
   }
+
 }
