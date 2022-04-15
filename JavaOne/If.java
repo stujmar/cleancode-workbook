c6c972c2ddded71ca2c9fce54822d9f3c85d40c3
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2022-04-14 21:48:18 -0500

    [sm] made If class to test if statements, probably bad to have keyword classes

---
+++ JavaOne/If.java
@@ -0,0 +1,13 @@
+package JavaOne;
+
+public class If {
+  public static void main(String[] args) {
+    int number = 5;
+    if (number <= 5) {
+      System.out.println("You are unlucky.");
+    } else {
+      System.out.println("You are lucky.");
+    }
+  }
+  
+}
