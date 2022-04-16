e02e3022b761224bf9937f09a9f65d79ea821e54
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2022-04-15 21:18:15 -0500

    [sm] got if working correctly in the scope of a psvm, wrote new scope class

---
+++ JavaOne/Scope.java
@@ -0,0 +1,21 @@
+package JavaOne;
+
+public class Scope {
+
+  public static void main(String[] args) {
+    int inputtedNum = 5;
+    if (inputtedNum < 100) {
+      int favoriteNumber = 5;
+      System.out.println("Your favorite number is " + favoriteNumber);
+      favoriteNumber = 10;
+      System.out.println("Your favorite number is " + favoriteNumber);
+      // In scope (accessible) for favoriteNumber
+    } else {
+      System.out.println("out of scope");
+      // Not in scope (not accessible) for favoriteNumber
+    }
+    // Not in scope (not accessible) for favoriteNumber
+    System.out.println("Still out of scope");
+  }
+
+}
